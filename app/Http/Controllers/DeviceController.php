7051bff62a31784a9c424146c5b398d92596a0ef
LordSnow <jeantshimbalanga9@gmail.com>
2024-08-18 22:12:30 +0100

    first commit

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    //
+    public function list($id=null){
+        $devices = Device::find($id);
+        return $id?$devices:Device::all();
+    }
+
+    public function create(Request $request){
+        $request->validate([
+            "name" =>["string", "required"],
+            "type" =>["string", "required"],
+            "manufacturer" =>["string", "required"],
+            "description" =>["string", "required"]
+        ]);
+        Device::create([
+            "name"=>$request->name,
+            "type"=>$request->type,
+            "manufacturer"=>$request->manufacturer,
+            "description"=>$request->description
+        ]);
+    }
+
+    public function update(Request $request, $id){
+        $device = Device::find($id);
+        if($device){
+            $device->update([
+                "name"=>$request->name
+            ]);
+            return $device;
+        }
+        return response()->json(['message'=>'Device not found'], 404);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\DeviceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('list/{id?}', [DeviceController::class, 'list']);
+Route::post('/create', [DeviceController::class, 'create']);
+Route::put('/update/{id}', [DeviceController::class, 'update']);
